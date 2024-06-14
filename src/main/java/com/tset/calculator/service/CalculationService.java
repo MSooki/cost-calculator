594c3ec08f4ea97c2148602d3ad97def1a90b960
Mihály Soóki <mihalysooki@Mihalys-MacBook-Pro.local>
2024-06-14 21:32:47 +0200

    simplify

--- src/main/java/com/tset/calculator/service/CalculationService.java
+++ src/main/java/com/tset/calculator/service/CalculationService.java
@@ -8,18 +8,16 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service
 public class CalculationService {
 
-    private ConcurrentHashMap<String, Float> results = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, Float> results = new ConcurrentHashMap<>();
 
     @Async
     public void calculate(String requestId, float principal, float annualInterestRate, int timesPerYear, int years) {
         try {
-            // Simulate a long calculation
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
 
-        // Compound interest formula: A = P(1 + r/n)^(nt)
         float rate = annualInterestRate / 100;
         float A = (float) (principal * Math.pow(1 + rate / timesPerYear, timesPerYear * years));
         results.put(requestId, A);
